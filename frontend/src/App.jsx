36817b79db805ab9763dea9dea2b50a3b7e8e818
seanlumasag <seanlumasag15@gmail.com>
2025-06-08 15:15:12 -0400

    A

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Home from './components/Home'
+import About from './components/About'
+import Experience from './components/Experience'
+import Contact from './components/Contact'
+
+function App() {
+  return (
+      <>
+        <h1>Hello World!</h1>
+        <Home />
+      </> 
+  )
+}
+
+export default App
---
+++ frontend/src/components/Experience.jsx
@@ -0,0 +1,9 @@
+function Experience(){
+    return (
+        <>
+            <h1>Experience</h1>
+        </>
+    )
+}
+
+export default Experience
