d8a4f115d1c92363a85dbe0b9da4b9d5f635e28d
seanlumasag <seanlumasag15@gmail.com>
2025-06-08 21:44:05 -0400

    A

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,18 @@
+import headshot from "../assets/headshot.jpg";
+
 function Home() {
   return (
     <div id="home">
-      <h1>Home</h1>
+      <div id="intro">
+        <p id="p1">Hello, I'm</p>
+        <p id="p2">Sean Lumasag</p>
+        <p id="p3">Software Engineer</p>
+        <div id="intro-buttons">
+          <a href="">Download Resume</a>
+          <a href="">Contact</a>
+        </div>
+      </div>
+      <img src={headshot} id="headshot" />
     </div>
   );
 }
