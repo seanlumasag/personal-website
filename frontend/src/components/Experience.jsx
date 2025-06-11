9927117ba36faa484df4ddd3569cc4de31d7b827
seanlumasag <seanlumasag15@gmail.com>
2025-06-11 17:49:57 -0400

    -

--- frontend/src/components/Experience.jsx
+++ frontend/src/components/Experience.jsx
@@ -1,21 +1,27 @@
 import ExperienceCard from "./ExperienceCard";
+import dataStorytellingImg from "../assets/data-storytelling.png";
+import hadoopExpressImg from "../assets/hadoop-express.png";
+import tagOnlineImg from "../assets/tag-online.png";
 
 function Experience() {
   return (
     <div className="experience" id="experience">
-      <h1>Experience</h1>
+      <h1 className="experience-title">Experience</h1>
       <div className="card-container">
         <ExperienceCard
+          img={dataStorytellingImg}
+          link="https://devonmcguinness.com"
           experienceTitle="Software Engineering Intern"
-          experienceCompany="Data Storytelling"
         />
         <ExperienceCard
+          img={hadoopExpressImg}
+          link="https://www.hadoopexpress.com"
           experienceTitle="Coding Instructor"
-          experienceCompany="Hadoop Express"
         />
         <ExperienceCard
+          img={tagOnlineImg}
+          link="https://tagonline.com"
           experienceTitle="Software Engineering Intern"
-          experienceCompany="TAG Online"
         />
       </div>
     </div>
--- frontend/src/components/ExperienceCard.jsx
+++ frontend/src/components/ExperienceCard.jsx
@@ -1,8 +1,14 @@
-function ExperienceCard({ experienceTitle, experienceCompany }) {
+import dataStorytellingImg from "../assets/data-storytelling.png";
+
+function ExperienceCard({ img, link, experienceTitle }) {
   return (
     <div className="experience-card">
+      <div className="logo-container">
+        <a href={link} target="_blank">
+          <img src={img} alt="" />
+        </a>
+      </div>
       <h1>{experienceTitle}</h1>
-      <h1>{experienceCompany}</h1>
     </div>
   );
 }
