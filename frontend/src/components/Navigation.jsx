b7d2f3152f3dabb9c899141ded9b0216b7e93f84
seanlumasag <seanlumasag15@gmail.com>
2025-06-16 19:06:03 -0400

    Improving responsiveness

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,28 +1,30 @@
 function Nav() {
   return (
-    <nav className="navigation section" id="navigation">
-      <a href="#home" className="name">
-        Sean <span className="accent">Lumasag</span>
-      </a>
-      <ul className="navbar">
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#experience">Experience</a>
-        </li>
-        <li>
-          <a href="#projects">Projects</a>
-        </li>
-        {/*
+    <nav className="navigation" id="navigation">
+      <div className="navbar-container">
+        <a href="#home" className="name">
+          Sean <span className="accent">Lumasag</span>
+        </a>
+        <ul className="navbar">
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#experience">Experience</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          {/*
         <li>
           <a href="#skills">Skills</a>
         </li>
         */}
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
