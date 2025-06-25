aac14bcf9c07339155a42f99eae878f1578eba71
seanlumasag <seanlumasag15@gmail.com>
2025-06-24 21:11:35 -0400

    Styling

--- frontend/src/sections/SectionFooter.jsx
+++ frontend/src/sections/SectionFooter.jsx
@@ -1,6 +1,7 @@
 function Footer() {
   return (
     <footer className="section section-footer" id="footer">
+      <p>Copyright © 2025 Sean Lumasag. All rights reserved.</p>
     </footer>
   );
 }
