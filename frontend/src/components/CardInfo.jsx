457ab3b145cc517f6460692afe0a2d888b652fd8
seanlumasag <seanlumasag15@gmail.com>
2025-07-14 22:15:55 -0400

    Responiveness for project buttons

--- frontend/src/components/CardInfo.jsx
+++ frontend/src/components/CardInfo.jsx
@@ -31,16 +31,18 @@ function CardInfo({
         <h3>{title}</h3>
 
         <p className="desc">{desc}</p>
-        <div className="tech-stack">
-          {t1 && <TechTag text={t1} />}
-          {t2 && <TechTag text={t2} />}
-          {t3 && <TechTag text={t3} />}
-          {t4 && <TechTag text={t4} />}
-          {t5 && <TechTag text={t5} />}
-          {t6 && <TechTag text={t6} />}
-          {t7 && <TechTag text={t7} />}
-          {t8 && <TechTag text={t8} />}
-          {t9 && <TechTag text={t9} />}
+        <div className="tag-container">
+          <div className="tech-stack">
+            {t1 && <TechTag text={t1} />}
+            {t2 && <TechTag text={t2} />}
+            {t3 && <TechTag text={t3} />}
+            {t4 && <TechTag text={t4} />}
+            {t5 && <TechTag text={t5} />}
+            {t6 && <TechTag text={t6} />}
+            {t7 && <TechTag text={t7} />}
+            {t8 && <TechTag text={t8} />}
+            {t9 && <TechTag text={t9} />}
+          </div>
           <div className="project-button-container">
             {a1 && <ProjectButton text={a1} link={b1} />}
             {a2 && <ProjectButton text={a2} link={b2} />}
